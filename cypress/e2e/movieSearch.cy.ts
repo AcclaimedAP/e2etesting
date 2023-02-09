f2f43b5bbba6d1da126f57833e398ec97a4d2df0
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-09 18:33:54 +0100

    fixed fixture

---
+++ cypress/e2e/movieSearch.cy.ts
@@ -0,0 +1,16 @@
+
+
+beforeEach(() => {
+        cy.visit("http://localhost:1234");
+})
+
+describe("Testing intended behavior on search", () => {
+    it("API call should include correct movie call", () => {
+
+        cy.intercept("GET", "http://omdbapi.com/*",{ fixture: "movieCall" }).as("moviesAPI");
+        cy.get("#searchText").type("kung pow");
+
+        cy.get("#search").click();
+        cy.wait("@moviesAPI").its("request.url").should("contain", "kung%20pow");
+    });
+});

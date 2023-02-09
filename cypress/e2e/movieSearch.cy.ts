554428d2c0e47e955c4ec2a5fd6e7a65b87ba7b8
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-09 18:41:12 +0100

    movie search 💪

--- cypress/e2e/movieSearch.cy.ts
+++ cypress/e2e/movieSearch.cy.ts
@@ -4,13 +4,16 @@ beforeEach(() => {
     cy.visit("/");
 });
 
-describe("Testing intended behavior on search", () => {
-    it("API call should include correct movie call", () => {
 
-        cy.intercept("GET", "http://omdbapi.com/*",{ fixture: "movieCall" }).as("moviesAPI");
-        cy.get("#searchText").type("kung pow");
+describe("Testing intended behavior on search", () => { // ✅
+    it("API call should include correct movie call", () => {
+        const searchString = "kung pow";
+        cy.intercept("GET", "http://omdbapi.com/*", { fixture: "movieCall" }).as("moviesAPI");
+        cy.get("#searchText").type(searchString).should("have.value", searchString);
 
         cy.get("#search").click();
         cy.wait("@moviesAPI").its("request.url").should("contain", "kung%20pow");
+
+        cy.get("h3:first").should("contain.text", "kung pow");
     });
 });
